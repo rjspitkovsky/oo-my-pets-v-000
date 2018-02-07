fb5d999fbd2d2d349e74d918d3e45cbc60876cd5
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-07 22:05:44 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 class Cat
-  # code goes here
+
+attr_accessor :mood
+attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def mood
+    @mood
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,82 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+  attr_accessor  :pets, :name
+  attr_reader  :species
+
+  def initialize(species, pets = {fishes: [], cats: [], dogs: []})
+    @species = species
+    @pets = pets
+    @@owners << self
+  end
+
+  #CLASS METHODS
+
+  @@owners = []
+
+  def self.all
+    @@owners
+  end
+
+    def self.count
+      @@owners.size
+    end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  #HASH/ARRAY ADDITION
+
+  def buy_fish(fish_name)
+    @pets[:fishes] << Fish.new(fish_name)
+  end
+
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
+  end
+
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
+  end
+
+  #HASH/ARRAY MANIPULATION
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+    def play_with_cats
+      @pets[:cats].each do |cat|
+        cat.mood = "happy"
+      end
+    end
+
+    def feed_fish
+      @pets[:fishes].each do |fish|
+        fish.mood = "happy"
+      end
+    end
+
+    def sell_pets
+      @pets.each do |species, pets|
+        pets.each do |pet|
+          pet.mood = "nervous"
+        end
+      end
+      @pets = {}
+    end
+
+    def list_pets
+      "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+    end
+
+
 end
