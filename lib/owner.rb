65c2848465ffca552ba4c043c13ac1b0d6eb81cd
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-07 20:38:06 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,16 +1,5 @@
 require 'pry'
 
 class Owner
-attr_accessor :name
 
-  @@owners = []
-
-    def initalize
-      @@owners << self
-    end
-
-    def self.all
-      @@owners
-    end
-binding.pry
 end
